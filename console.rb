c5066ef9b6b4d0dab7388f6bbba5949a07083aa3
Alison Wood <alisonwood29@gmail.com>
2017-12-05 16:54:35 +0000

    added methods for deleting and finding

--- console.rb
+++ console.rb
@@ -3,11 +3,20 @@ require_relative('./models/bounty_tracker')
 
 
 space_cowboy_1_p = {"name" => "Predator", "bounty_value" => 10000, "danger_level" => "XXXXXX DANGEROUS", "still_wanted" => true}
-space_cowboy_1 = BountyTracker.new(space_cowboy_1_p)
-space_cowboy_1.save()
 
-puts space_cowboy_1.still_wanted
+space_cowboy_2_p = {"name" => "Donald Trump", "bounty_value" => 5000000, "danger_level" => "Equal to Kim Jong Un", "still_wanted" => false}
+
+space_cowboy_1 = BountyTracker.new(space_cowboy_1_p)
+space_cowboy_2 = BountyTracker.new(space_cowboy_2_p)
+space_cowboy_1.save()
+space_cowboy_2.save()
+
 puts space_cowboy_1.id
 
 space_cowboy_1.name = "Eric"
 space_cowboy_1.update
+
+#space_cowboy_2.delete
+
+eric = BountyTracker.find("Eric")
+p eric
--- models/bounty_tracker.rb
+++ models/bounty_tracker.rb
@@ -14,12 +14,12 @@ class BountyTracker
     @still_wanted = options["still_wanted"]
   end
 
-  def create_connection()
+  def self.create_connection()
     return PG.connect({dbname: 'space_cowboys', host:'localhost'})
   end
 
   def save()
-    db = create_connection()
+    db = BountyTracker.create_connection()
     sql = "INSERT INTO space_cowboys (name, bounty_value, danger_level, still_wanted)
           VALUES ($1, $2, $3, $4) RETURNING id"
 
@@ -33,13 +33,34 @@ class BountyTracker
   end
 
   def update()
-    db = create_connection()
+    db = BountyTracker.create_connection()
     sql = "UPDATE space_cowboys SET (name, bounty_value, danger_level, still_wanted)
       = ($1, $2, $3, $4) WHERE id = $5"
     values = [@name, @bounty_value, @danger_level, @still_wanted, @id]
     db.prepare("update", sql)
     db.exec_prepared("update", values)
-    db.close
+    db.close()
   end
+
+  def delete()
+    db = BountyTracker.create_connection()
+    sql = "DELETE FROM space_cowboys WHERE id = $1"
+    values = [@id]
+    db.prepare("delete_one", sql)
+    db.exec_prepared("delete_one", values)
+    db.close()
+  end
+
+  def self.find(name)
+    db = BountyTracker.create_connection()
+    sql = "SELECT name, bounty_value, danger_level, still_wanted FROM space_cowboys
+          WHERE name = $1 "
+    values = [name]
+    db.prepare("find", sql)
+    bounty = db.exec_prepared("find", values)
+    db.close()
+    return bounty.map{|bounty| BountyTracker.new(bounty)}[0]
+  end
+
 
 end
